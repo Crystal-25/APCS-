71d950a003339af081b5c7f491ecd5d7f3e1f957
Crystal-25 <JunyaoYang25@marlborough.org>
2022-10-06 14:38:18 -0700

    unit2

--- unit2/Makes10.java
+++ unit2/Makes10.java
@@ -1,6 +1,8 @@
 public class Makes10{
   public static void main(String[] args){
-    testmakes10();
+    testmakes10(20, 10, false);
+    testmakes10(9, 8, true);
+    testmakes10(10, 12, true);
     }
   public static boolean makes10(int a, int b) {
   if (a == 10 || a + b == 10 || b == 10){
@@ -10,9 +12,16 @@ public class Makes10{
     return false;
     }
   }
-  public static void testmakes10(){
-    System.out.println(makes10(9, 9));
-    System.out.println(makes10(9,1));
-  }
+  public static void testmakes10(int a, int b, boolean exp){
+    //exp means excepted, it is what we think it output is
+    boolean ans = makes10(a, b);
 
-}
+    System.out.println("a: " + a + " " + " b: " + b + " exp: " + exp + " ans:" + ans);
+
+    if (ans == exp)
+      System.out.println(" correct ");
+    else
+      System.out.println(" wrong ");
+      //you don't need curly brackets because it is only a single statement
+    }
+  }
