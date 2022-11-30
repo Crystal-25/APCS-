36411a8cd95915e100b3a84e4957d814403659e4
Crystal-25 <JunyaoYang25@marlborough.org>
2022-11-30 08:54:38 -0800

    unit 6

---
+++ unit6/Smile.java
@@ -0,0 +1,38 @@
+import processing.core.*;
+
+public class Smile {
+  int x;
+  int y;
+  int s;
+
+Smile(PApplet p, int x, int y, int s){
+  this.p = p;
+  x = int(p.random(p.width));
+  y = int(p.random(p.length));
+  s = int(p.random(p.size));
+
+  }
+void display(){
+    //head
+    p.strokeWeight(3);
+    p.fill(242, 224, 59);
+    p.ellipse(x, y, s, s);
+    //eyes
+    p.fill(0);
+    p.ellipse(x+s/6, y-s/6, s/4, s/4);
+    p.ellipse(x-s/6, y-s/6, s/4, s/4);
+    //pupils
+    p.stroke(255);
+    p.fill(255);
+    p.ellipse(x+s/8, y-s/5, s/8, s/8);
+    p.ellipse(x-s/8, y-s/5, s/8, s/8);
+
+    //mouth
+    p.strokeWeight(3);
+    p.noFill();
+    p.stroke(0);
+    p.arc(x, y+s/12, s/2, s/2, 0, PI);
+    p.line(x-s/4, y+s/12, x+s/4, y+s/12);
+
+  }
+}
