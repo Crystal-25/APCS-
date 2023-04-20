edf714c1bed99ca32b1ab13bcb25422f68454b7a
Crystal-25 <JunyaoYang25@marlborough.org>
2023-04-20 08:36:34 -0700

    project

---
+++ Project/Main.java
@@ -0,0 +1,111 @@
+import processing.core.*;
+import java.util.*;  //import it when you use an
+//arraylist or special tools
+
+public class Main extends PApplet{
+
+  ArrayList<Tetris> tetrises;
+  Tetris curr;
+
+  public void settings(){
+
+    fullScreen();
+
+  }
+  public void setup(){
+
+    tetrises = new ArrayList<Tetris>();
+    createTetris();
+  }
+
+  public void createTetris(){
+
+    Tetris t = new Tetris(this);
+    tetrises.add(t);
+
+  }
+
+  public void drop()
+  {
+    if(tetrises.get(tetrises.size() - 1).groundStop()){
+      createTetris();
+    }
+
+    /*for(int i = 0; i < tetrises.size(); i++){
+      if(stop(tetrises.get(i))){
+        createTetris();
+    }
+  }
+  */
+}
+
+/*public boolean stop(Tetris block){
+
+  if(tetrises.get(tetrises.size() - 1).pos.x + 100 > block.pos.x
+  && tetrises.get(tetrises.size() - 1).pos.x < block.pos.x + 100
+  && tetrises.get(tetrises.size() - 1).pos.y + 100 > block.pos.y
+  && tetrises.get(tetrises.size() - 1).pos.y < block.pos.y + 100){
+
+    return true;
+  }
+
+  return false;
+
+}
+*/
+  public void draw(){
+
+    background(0);
+    drop();
+
+    for(Tetris t: tetrises){
+      t.display();
+      t.move();
+
+    /*  if(collision(t))
+      {
+        drop();
+
+      }
+      */
+    }
+  }
+
+  public boolean collision(Tetris curr){
+
+    for(Tetris t: tetrises){
+
+      if(t.pos.x == curr.pos.x && t.pos.y > curr.pos.y){
+
+        createTetris();
+        return true;
+        //make curr stay
+      }
+    }
+
+    return false;
+  }
+
+  public void keyPressed(){
+
+    System.out.println("");
+
+    for(Tetris t: tetrises){
+
+        if(t.getSele() == true){
+          if(keyCode == LEFT)
+            t.moveLeft();
+
+          else if(keyCode == RIGHT)
+            t.moveRight();
+
+          else if(keyCode == DOWN)
+            t.moveDown();
+          }
+      }
+  }
+
+  public static void main(String[] args){
+    PApplet.main("Main");
+  }
+}
---
+++ Project/Tetris.java
@@ -0,0 +1,101 @@
+import processing.core.*;
+
+public class Tetris{
+
+  private PApplet p;
+  public PVector pos;
+  private boolean sele;
+  private String diffshape;
+
+  private int x;
+  private int y;
+  private int speed;
+
+  public boolean getSele(){
+    return sele;
+  }
+
+  public Tetris(PApplet p){
+
+    this.p = p;
+    //this.diffshape = diffshape;
+    pos = new PVector(p.width/2, 0);
+
+    sele = true;
+
+    float f = p.random(4);
+
+    if(f < 1)
+      diffshape = "line";
+    else if(f < 2)
+      diffshape = "twist";
+    else if(f < 3)
+      diffshape = "L";
+    else if(f < 4)
+      diffshape = "square";
+
+  }
+
+  public void move(){
+    pos.y += 2;
+    if(pos.y>p.height - 200)
+      pos.y = p.height-200;
+
+  }
+
+  public boolean groundStop(){
+
+  if(pos.y + 50 > p.height - 200){
+      pos.y = p.height - 200;
+        sele = false;
+        return true;
+    }
+    return false;
+  }
+
+  public void display(){
+    p.fill(255);
+    //p.rect(pos.x, pos.y, 50, 50);
+    //x = x + speed;
+
+    //if( x >= p.width || x<=0)
+
+    if( diffshape.equals("square")){
+      p.rect(pos.x, pos.y, 100, 100);
+    }
+
+    if( diffshape.equals("line")){
+      p.rect(pos.x, pos.y, 50, 200);
+    }
+
+    if( diffshape.equals("twist")){
+      p.rect(pos.x, pos.y, 50, 100);
+      p.rect(pos.x + 50, pos.y + 50, 50, 100);
+    }
+
+    if( diffshape.equals("L")){
+      p.rect(pos.x, pos.y, 50, 150);
+      p.rect(pos.x + 50, pos.y + 50, 50, 50);
+    }
+  }
+
+  public void moveLeft(){
+    pos.x -= 50;
+  }
+
+  public void moveRight(){
+    pos.x += 50;
+  }
+
+  public void moveDown(){
+    pos.y += 50;
+  }
+
+  Tetris(int x, int y, int speed){
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    //this.moveLeft = moveLeft;
+  }
+
+}
